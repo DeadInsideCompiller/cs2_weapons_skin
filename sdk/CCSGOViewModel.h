80401531895bd3719b8a9c577bbf1aba4bbd8914
DeadInsideCompiller <157217591+DeadInsideCompiller@users.noreply.github.com>
2024-02-10 23:47:04 +0500

    Create CCSGOViewModel.h

---
+++ sdk/CCSGOViewModel.h
@@ -0,0 +1,32 @@
+#include "schemasystem.h"
+#include "baseentity.h"
+#include "CBaseFlex.h"
+#include "CBaseViewModel.h"
+
+//#include "CBodyComponent.h"
+#include "CPlayer_ItemServices.h"
+#include "CBodyComponentSkeletonInstance.h"
+
+
+struct Vector3 {
+	float x, y, z;
+};
+
+class CNetworkViewOffsetVector {
+public:
+    float x; // CNetworkedQuantizedFloat
+    float y; // CNetworkedQuantizedFloat
+    float z; // CNetworkedQuantizedFloat
+};
+class CBaseModelEntity;
+class CCSGOViewModel /*: public CBaseViewModel*/ {
+public:
+    SCHEMA_FIELD(CBodyComponent*, C_BaseEntity, m_CBodyComponent);
+    SCHEMA_FIELD(CBodyComponentSkeletonInstance*, C_BaseEntity, m_pGameSceneNode);
+
+    SCHEMA_FIELD(Vector3, CBaseFlex, m_vLookTargetPosition);
+	//m_vecLastFacing CBaseViewModel
+    SCHEMA_FIELD(Vector3, CBaseFlex, m_viewtarget);
+    //SCHEMA_FIELD(Vector3, CBaseFlex, m_CachedViewTarget);
+    SCHEMA_FIELD(CHandle<void*>, CBaseViewModel, m_iCameraAttachment);
+};
